e9773c98a0b2e7e4fa69dd854914d00c5c7650b2
kumbukaime <84021868+kumbukaime@users.noreply.github.com>
2021-06-19 23:14:15 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,34 @@
+//function compute to work out the calculation
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var year = new Date().getFullYear()+parseInt(years);
+    var interest = principal * years * rate /100;
+	
+
+    function updateRate() 
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
+    value.textContent = rate.value;
+
 }
-        
+
+//insert text for result after computing
+ document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+interest+",\<br\>in the year "+year+"\<br\>"
+ 
+ //principal should be greater than zero//
+    if (principal == 0 || principal <0) {
+        alert ("Enter a positive number");
+        return;
+    }
+
+//update slider value
+function updateSlider()
+{
+    document.getElementById("rate_val").innerHTML = "${rate}%";
+}
+}
+
